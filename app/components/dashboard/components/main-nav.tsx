ab7e070c925989aa1a7e615a513722253c113f74
Judy <judy.suparat.superdev@gmail.com>
2024-01-02 20:43:11 +0700

    Fix link to

--- app/components/dashboard/components/main-nav.tsx
+++ app/components/dashboard/components/main-nav.tsx
@@ -12,25 +12,25 @@ export function MainNav({
       {...props}
     >
       <Link
-        to="/examples/dashboard"
+        to="/dashboard"
         className="text-sm font-medium transition-colors hover:text-primary"
       >
         Overview
       </Link>
       <Link
-        to="/examples/dashboard"
+        to="/dashboard"
         className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
       >
         Customers
       </Link>
       <Link
-        to="/examples/dashboard"
+        to="/dashboard"
         className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
       >
         Products
       </Link>
       <Link
-        to="/examples/dashboard"
+        to="/dashboard"
         className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
       >
         Settings
